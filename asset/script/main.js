36bb4199abbf7efe4e14b87000e23d0b7946d2dc
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-03-17 11:39:38 +0900

    3回目のコミット

--- asset/script/main.js
+++ asset/script/main.js
@@ -6,11 +6,17 @@ $('.hamburger-button').on('click', function() {
   hamburger.toggleClass('hamburger-menu-active');
 
 });
+var box = $('.hamburger-menu');
 
+$('.hamburger-menu li').on('click', function() {
+  
+  hamburger.removeClass('hamburger-menu-active');
+});
 $(window).on('resize', function() {
 
   hamburger.removeClass('hamburger-menu-active');
 });
+
 $('#smarttab').smartTab({
   enableUrlHash: false 
 });
